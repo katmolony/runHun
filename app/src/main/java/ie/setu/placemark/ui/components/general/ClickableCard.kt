17a34da4d6da63bf0f53e9ccd10a52037677a040
Kate Molony <katemolony96@gmail.com>
2025-01-03 03:20:10 +0000

    Change option page.

--- app/src/main/java/ie/setu/placemark/ui/components/general/ClickableCard.kt
+++ app/src/main/java/ie/setu/placemark/ui/components/general/ClickableCard.kt
@@ -1,29 +1,49 @@
 package ie.setu.placemark.ui.components.general
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ClickableCard(
     text: String,
     onClick: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    icon: ImageVector? = null
 ) {
     Card(
         modifier = modifier
-            .padding(8.dp)
+            .padding(16.dp)
             .clickable { onClick() },
-        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
     ) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text(text = text)
+        Row(
+            modifier = Modifier
+                .padding(24.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            if (icon != null) {
+                Icon(
+                    imageVector = icon,
+                    contentDescription = null,
+                    modifier = Modifier.size(32.dp).padding(end = 8.dp)
+                )
+            }
+            Text(
+                text = text,
+                fontSize = 24.sp
+            )
         }
     }
 }
--- app/src/main/java/ie/setu/placemark/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/ie/setu/placemark/ui/screens/settings/SettingsScreen.kt
@@ -1,17 +1,26 @@
 package ie.setu.placemark.ui.screens.settings
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale.Companion.Crop
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import ie.setu.placemark.R
 import ie.setu.placemark.ui.components.general.ClickableCard
 import ie.setu.placemark.ui.components.general.HeadingTextComponent
@@ -30,6 +39,15 @@ fun SettingsScreen(
         verticalArrangement = Arrangement.SpaceEvenly
     ) {
         HeadingTextComponent(value = stringResource(id = R.string.settings_title))
+        Spacer(
+            modifier = Modifier.height(10.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.ic_settings),
+            contentDescription = null,
+            contentScale = Crop,
+            modifier = Modifier.clip(CircleShape).width(180.dp).height(180.dp)
+        )
         Spacer(modifier = Modifier.height(10.dp))
 
         ClickableCard(
